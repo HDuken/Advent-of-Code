4e3f2ad5de4fbc9e94a41deeff3247541de1ef03
Duke Nguyen <duke7295@gmail.com>
2022-12-18 11:20:55 +0100

    Add template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Advent-of-Code
+
 My solutions for Advent of Code
+
+- [Advent of Code 2022](AoC-2022/)
---
+++ temp/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://adventofcode.com/2022
+
+clear
+# Done: 
+# Undone: 
+g++ -Wall   day1-p1.cpp -o out
+./out <     day1-p1.txt
+rm -f out
