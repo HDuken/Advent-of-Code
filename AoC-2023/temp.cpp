0ad78ad5f2b48e6efe5c4c65d0a55c0f6baeb072
Duke Nguyen <duke7295@gmail.com>
2023-12-06 23:05:39 +0100

    AOC-3-Day-4 (#6)

--- AoC-2023/temp.cpp
+++ AoC-2023/temp.cpp
@@ -10,12 +10,41 @@
 
 #include <math.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <numeric>
 #include <map>
 
 using namespace std;
+
+const int ALPHABET = 26;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define endl        '\n'
+#define all(v)      v.begin(), v.end()
+#define ff          first
+#define ss          second
+
+// I/O
+#define get_arr(a, n)   for(int i = 0; i < n; i++)  cin >> a[i];
+#define LOG(x)          cerr << #x << " = " << x << endl;
+template <typename T, typename...Ts>
+constexpr void print(T&& first, Ts&&... rest) noexcept {
+    if constexpr (sizeof...(Ts) == 0)   cout << first << endl;
+    else {  cout << first << ' '; print(std::forward<Ts>(rest)...); }
+}
+template <typename T>
+void print(T a[], int n) {
+    for(int i = 0; i < n; i++)  cout << a[i] << " \n"[i == n - 1];
+}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& vect) noexcept {
+    for (const T& v : vect) { os << v << ' '; } os << endl;
+    return os;
+}
 
 int ans = 0;
 string line;
