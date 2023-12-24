071582fcc81dadeb2f7d84c18c6183a393d14dba
Duke Nguyen <duke7295@gmail.com>
2023-12-24 09:38:18 +0100

    Add solution day 9

--- AoC-2023/9.cpp
+++ AoC-2023/9.cpp
@@ -50,16 +50,48 @@ ostream& operator<<(ostream& os, const vector<T>& vect) noexcept {
 int ans = 0;
 stringstream ss;
 string line;
+vector<vector<int>> inputs;
+int x;
 
 void read_input()
 {
     while (getline(cin, line)) {
-        cout << line << endl;
+        ss.str("");
+        ss.clear();
+        ss << line;
+
+        vector<int> v;
+        while (ss >> x)
+            v.push_back(x);
+        inputs.push_back(v);
     }
+}
+
+bool good(const vector<int> seq) {
+    for (const int& i : seq) {
+        if (i != 0)
+            return false;
+    }
+    return true;
 }
 
 void solve()
 {
+    for (vector<int>& seq : inputs) {
+        int sign = 1;
+        do {
+#if PART1
+            ans += seq.back();
+#else
+            ans += seq.front() * sign;
+            sign *= -1;
+#endif // PART1
+            vector<int> diff;
+            for (int i = 1; i < seq.size(); i++)
+                diff.push_back(seq[i] - seq[i - 1]);
+            swap(seq, diff);
+        } while (!good(seq));
+    }
     cout << ans << endl;
 }
 
