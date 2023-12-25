0959242765427868f90ebc15bfcb22864c91cc76
Duke Nguyen <duke7295@gmail.com>
2023-12-25 10:10:24 +0100

    Init solution day 11

--- AoC-2023/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-clear
-touch $1.txt
-cp temp.cpp $1.cpp
--- AoC-2023/run.sh
+++ AoC-2023/run.sh
@@ -2,6 +2,14 @@
 
 clear
 echo "Advent of Code 2023 | Day $1 Part $2:"
+
+if [ ! -e $1.cpp ]; then
+    echo "Initialize Day $1"
+    cp temp.cpp $1.cpp
+    touch $1.txt
+    exit
+fi
+
 g++ -DPART"$2"=true "$1".cpp -o out &&
 ./out < "$1".txt &&
 rm -f out
