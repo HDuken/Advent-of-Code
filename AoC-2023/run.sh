23ff7f0d1f88c9e05a80ce6796e77e418d8e9b81
Duke Nguyen <49876068+duken72@users.noreply.github.com>
2024-12-01 08:53:25 +0100

    AOC-11-Day-12 (#14)
    * Init solution day 12
    
    * Backup
    
    ---------
    
    Co-authored-by: Duke Nguyen <duke7295@gmail.com>
    Co-authored-by: Huu Duc Nguyen <tech.hodapp@simplelogin.com>

--- AoC-2023/run.sh
+++ AoC-2023/run.sh
@@ -6,10 +6,10 @@ echo "Advent of Code 2023 | Day $1 Part $2:"
 if [ ! -e $1.cpp ]; then
     echo "Initialize Day $1"
     cp temp.cpp $1.cpp
-    touch $1.txt
+    touch "$1"real.txt "$1"exam.ex
     exit
 fi
 
-g++ -DPART"$2"=true "$1".cpp -o out &&
+g++ -DPART"$2"=true "$1".cpp -DDEBUG="$3" -o out &&
 ./out < "$1".txt &&
 rm -f out
