af9474901cf80d38dafc0d464f0d6a7d9158ee58
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-12-01 10:47:00 +0100

    Refactor files and update precompile header file

--- pch.hpp
+++ pch.hpp
@@ -1,31 +1,36 @@
-#ifndef AOC__PCH_HPP_
-#define AOC__PCH_HPP_
+/*
+ *   Copyright (c) 2022 Duc Huu Nguyen
+ *   All rights reserved.
+ */
+
+#ifndef AOC_2024_PCH_HPP_
+#define AOC_2024_PCH_HPP_
 
 #pragma GCC optimize("O2,unroll-loops")
 // #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 #if PART1
 #define PART2 false
-#endif // PART1
+#endif  // PART1
 #if PART2
 #define PART1 false
-#endif // PART2
+#endif  // PART2
 
-#include <cmath>
-#include <array>
-#include <vector>
-#include <iostream>
-#include <ostream>
-#include <strstream>
-#include <numeric>
-#include <sstream>
 #include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <string>
 #include <utility>
+#include <vector>
 
-using namespace std;
+using namespace std;  // NOLINT
 
 const int ALPHABET = 26;
-typedef long long ll;
+typedef long long ll;  // NOLINT
 typedef pair<int, int> pii;
 
 #define endl        '\n'
@@ -34,21 +39,27 @@ typedef pair<int, int> pii;
 #define ss          second
 
 // I/O
-#define get_arr(a, n)   for(int i = 0; i < n; i++)  cin >> a[i];
+#define get_arr(a, n)   for (int i = 0; i < n; i++)  cin >> a[i];
 #define LOG(x)          cerr << #x << " = " << x << endl;
 template <typename T, typename...Ts>
 constexpr void print(T&& first, Ts&&... rest) noexcept {
-    if constexpr (sizeof...(Ts) == 0)   cout << first << endl;
-    else {  cout << first << ' '; print(std::forward<Ts>(rest)...); }
+    if constexpr (sizeof...(Ts) == 0) {
+        cout << first << endl;
+    } else {
+        cout << first << ' ';
+        print(std::forward<Ts>(rest)...); }
 }
 template <typename T>
 void print(T a[], int n) {
-    for(int i = 0; i < n; i++)  cout << a[i] << " \n"[i == n - 1];
+    for (int i = 0; i < n; i++)  cout << a[i] << " \n"[i == n - 1];
 }
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T>& vect) noexcept {
-    for (const T& v : vect) { os << v << ' '; } os << endl;
+    for (const T& v : vect) {
+        os << v << ' ';
+    }
+    os << endl;
     return os;
 }
 
-#endif  // AOC__PCH_HPP_
+#endif  // AOC_2024_PCH_HPP_
