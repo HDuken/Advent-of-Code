1fb12cfc95b18d310981fd7867acbb07c37b441c
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-12-01 10:23:05 +0100

    Format CPP

--- AoC-2024/1.cpp
+++ AoC-2024/1.cpp
@@ -9,10 +9,10 @@
 
 #if PART1
 #define PART2 false
-#endif // PART1
+#endif  // PART1
 #if PART2
 #define PART1 false
-#endif // PART2
+#endif  // PART2
 
 #include <algorithm>
 #include <array>
@@ -38,29 +38,29 @@ typedef pair<int, int> pii;
 
 // I/O
 #define get_arr(a, n)                                                          \
-  for (int i = 0; i < n; i++)                                                  \
-    cin >> a[i];
+    for (int i = 0; i < n; i++)                                                  \
+        cin >> a[i];
 #define LOG(x) cerr << #x << " = " << x << endl;
 template <typename T, typename... Ts>
 constexpr void print(T &&first, Ts &&...rest) noexcept {
-  if constexpr (sizeof...(Ts) == 0)
-    cout << first << endl;
-  else {
-    cout << first << ' ';
-    print(std::forward<Ts>(rest)...);
-  }
+    if constexpr (sizeof...(Ts) == 0) {
+        cout << first << endl;
+    } else {
+        cout << first << ' ';
+        print(std::forward<Ts>(rest)...);
+    }
 }
 template <typename T> void print(T a[], int n) {
-  for (int i = 0; i < n; i++)
-    cout << a[i] << " \n"[i == n - 1];
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " \n"[i == n - 1];
 }
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &vect) noexcept {
-  for (const T &v : vect) {
-    os << v << ' ';
-  }
-  os << endl;
-  return os;
+    for (const T &v : vect) {
+        os << v << ' ';
+    }
+    os << endl;
+    return os;
 }
 
 ll ans = 0;
@@ -70,39 +70,39 @@ vector<ll> ids_1;
 vector<ll> ids_2;
 
 void read_input() {
-  while (getline(cin, line)) {
-    stringstream ss(line);
-    ss.str(line);
-    ss >> a >> b;
-    ids_1.push_back(a);
-    ids_2.push_back(b);
-  }
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        ss.str(line);
+        ss >> a >> b;
+        ids_1.push_back(a);
+        ids_2.push_back(b);
+    }
 }
 
 void solve() {
-  sort(ids_1.begin(), ids_1.end());
-  sort(ids_2.begin(), ids_2.end());
+    sort(ids_1.begin(), ids_1.end());
+    sort(ids_2.begin(), ids_2.end());
 
 #if PART1
-  for (int i = 0; i < ids_1.size(); i++) {
-    ans += abs(ids_2[i] - ids_1[i]);
-  }
+    for (int i = 0; i < ids_1.size(); i++) {
+        ans += abs(ids_2[i] - ids_1[i]);
+    }
 #else
   auto it2 = ids_2.begin();
   for (int i = 0; i < ids_1.size(); i++) {
-    while (*(it2) < ids_1[i] && it2 != ids_2.end()) {
-      it2++;
-    }
-    int cnt = count(it2, ids_2.end(), ids_1[i]);
-    ans += ids_1[i] * cnt;
+      while (*(it2) < ids_1[i] && it2 != ids_2.end()) {
+          it2++;
+      }
+      int cnt = count(it2, ids_2.end(), ids_1[i]);
+      ans += ids_1[i] * cnt;
   }
-#endif // PART1
+#endif  // PART1
 
-  LOG(ans);
+    LOG(ans);
 }
 
 int main(int argc, char const *argv[]) {
-  read_input();
-  solve();
-  return 0;
+    read_input();
+    solve();
+    return 0;
 }
--- AoC-2024/temp.cpp
+++ AoC-2024/temp.cpp
@@ -8,10 +8,10 @@
 
 #if PART1
 #define PART2 false
-#endif // PART1
+#endif  // PART1
 #if PART2
 #define PART1 false
-#endif // PART2
+#endif  // PART2
 
 #include <algorithm>
 #include <array>
@@ -36,29 +36,29 @@ typedef pair<int, int> pii;
 
 // I/O
 #define get_arr(a, n)                                                          \
-  for (int i = 0; i < n; i++)                                                  \
-    cin >> a[i];
+    for (int i = 0; i < n; i++)                                                  \
+        cin >> a[i];
 #define LOG(x) cerr << #x << " = " << x << endl;
 template <typename T, typename... Ts>
 constexpr void print(T &&first, Ts &&...rest) noexcept {
-  if constexpr (sizeof...(Ts) == 0)
-    cout << first << endl;
-  else {
-    cout << first << ' ';
-    print(std::forward<Ts>(rest)...);
-  }
+    if constexpr (sizeof...(Ts) == 0)
+        cout << first << endl;
+    else {
+        cout << first << ' ';
+        print(std::forward<Ts>(rest)...);
+    }
 }
 template <typename T> void print(T a[], int n) {
-  for (int i = 0; i < n; i++)
-    cout << a[i] << " \n"[i == n - 1];
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " \n"[i == n - 1];
 }
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &vect) noexcept {
-  for (const T &v : vect) {
-    os << v << ' ';
-  }
-  os << endl;
-  return os;
+    for (const T &v : vect) {
+        os << v << ' ';
+    }
+    os << endl;
+    return os;
 }
 
 int ans = 0;
@@ -66,15 +66,17 @@ stringstream ss;
 string line;
 
 void read_input() {
-  while (getline(cin, line)) {
-    cout << line << endl;
-  }
+    while (getline(cin, line)) {
+        cout << line << endl;
+    }
 }
 
-void solve() { cout << ans << endl; }
+void solve() {
+    cout << ans << endl;
+}
 
 int main(int argc, char const *argv[]) {
-  read_input();
-  solve();
-  return 0;
+    read_input();
+    solve();
+    return 0;
 }
