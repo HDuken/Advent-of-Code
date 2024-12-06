0565cfd62e03ac56eef1350a1209b79073f2d1c8
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-12-06 12:39:12 +0100

    Back up draft for 2nd half

--- AoC-2024/6.cpp
+++ AoC-2024/6.cpp
@@ -4,7 +4,6 @@
  */
 
 #include "../pch.hpp"
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -18,10 +17,11 @@ const char D[] = "^>v<";
 int x = -1, y = -1;
 int d;
 
+using point_triplet = tuple<pii, pii, pii>;
+
 void read_input() {
     string line;
     while (getline(cin, line)) {
-        LOG(line);
         m.push_back(line);
 
         if (x == -1 && y == -1) {
@@ -37,33 +37,26 @@ void read_input() {
     }
     n_row = m.size();
     n_col = m[0].size();
-    LOG(n_row);
-    LOG(n_col);
-    LOG(x);
-    LOG(y);
-    LOG(d);
 }
 
 void solve() {
+    int x_new, y_new;
+#if PART1
+    // Simulate the roll-out
     while (x >= 0 && y >= 0 && x < n_row && y < n_col) {
-        LOG(x);
-        LOG(y);
+        // Mark visited positions
         m[x][y] = 'X';
         // The next position
-        int x_new = x;
-        int y_new = y;
-        switch (d) {
-        case 0:
-            x_new--;
-        case 1:
-            y_new++;
-        case 2:
-            x_new++;
-        case 3:
-            y_new--;
-        }
+        x_new = x;
+        y_new = y;
+        if (d == 0) x_new--;
+        else if (d == 1) y_new++;
+        else if (d == 2) x_new++;
+        else if (d == 3) y_new--;
 
-        if (m[x_new][y_new] == '#') {
+        if (x_new == -1 || x_new == n_row || y_new == -1 || y_new == n_col) {
+            break;
+        } else if (m[x_new][y_new] == '#') {
             d = (d + 1) % 4;
             x_new = x;
             y_new = y;
@@ -73,11 +66,75 @@ void solve() {
         }
     }
 
+    // Count visited positions
     for (int i = 0; i < n_row; i++) {
         for (int j = 0; j < n_col; j++) {
             ans += m[i][j] == 'X';
         }
     }
+#else
+    // vector of intersected points
+    vector<pii> p;
+
+    while (x >= 0 && y >= 0 && x < n_row && y < n_col) {
+        // The next position
+        x_new = x;
+        y_new = y;
+        if (d == 0) x_new--;
+        else if (d == 1) y_new++;
+        else if (d == 2) x_new++;
+        else if (d == 3) y_new--;
+
+        if (x_new == -1 || x_new == n_row || y_new == -1 || y_new == n_col) {
+            break;
+        } else if (m[x_new][y_new] == '#') {
+            d = (d + 1) % 4;
+            x_new = x;
+            y_new = y;
+            m[x][y] = '+';
+            p.push_back(pii(x, y));
+        } else {
+            x = x_new;
+            y = y_new;
+            if (m[x][y] == '.') {
+                m[x][y] = (d % 2 == 0 ? '|' : '-');
+            } else if (m[x][y] == '|' && d % 2 == 1) {
+                m[x][y] = '+';
+                p.push_back(pii(x, y));
+            } else if (m[x][y] == '-' && d % 2 == 0) {
+                m[x][y] = '+';
+                p.push_back(pii(x, y));
+            }
+        }
+    }
+
+    for (auto line : m) {
+        LOG(line);
+    }
+
+    sort(all(p), [](pii p1, pii p2) {
+        if (p1.ff == p2.ff) return p1.ss < p2.ss;
+        return p1.ff < p2.ff;
+    });
+    // for (pii pts1 : p) {
+    //     cout << pts1.ff << ' ' << pts1.ss << endl;
+    // }
+    // Find all triplets of intersected points
+    for (pii pts1 : p) {
+        for (pii pts2 : p) {
+            if (pts2.ff != pts1.ff || pts2.ss <= pts1.ss) continue;
+            for (pii pts3 : p) {
+                if (pts3.ff == pts1.ff) continue;
+                if (pts3.ss != pts1.ss && pts3.ss != pts2.ss) continue;
+                // cout << "Triplet:" << endl;
+                // cout << pts1.ff << ' ' << pts1.ss << endl;
+                // cout << pts2.ff << ' ' << pts2.ss << endl;
+                // cout << pts3.ff << ' ' << pts3.ss << endl;
+            }
+        }
+    }
+
+#endif  // PART1
 }
 
 int main(int argc, char const *argv[]) {
