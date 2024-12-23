2b7dbb1119839c286bc3cfce6e3536d54d6e19f1
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2024-12-24 00:27:34 +0100

    Back up draft for day 24

---
+++ AoC-2024/24.cpp
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (c) 2024 Duc Huu Nguyen
+ *   All rights reserved.
+ */
+
+#include "../pch.hpp"
+
+int ans = 0;
+int n_row = 0, n_col = 0;
+pii ds[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+stringstream ss;
+
+void read_input() {
+    string line;
+    while (getline(cin, line)) {
+        LOG(line);
+        ss.clear();
+        ss.str(line);
+    }
+}
+
+void solve() {
+    // TODO(Duck): duck
+}
+
+int main(int argc, char const *argv[]) {
+    read_input();
+    solve();
+    LOG(ans);
+    return 0;
+}
