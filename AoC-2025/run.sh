c677db7445efe3f48e99702e56af2496061c77d2
Duke <49876068+duken72@users.noreply.github.com>
2025-12-04 15:57:38 +0100

    Add solution for day 1, 2025 (#40)
    Co-authored-by: Huu Duc Nguyen <tech.hodapp@simplelogin.com>

---
+++ AoC-2025/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo "Advent of Code 2025 | Day $1:"
+
+if [ ! -e $1.cpp ]; then
+    echo "Initialize Day $1"
+    cp temp.cpp $1.cpp
+    touch $1.txt
+    exit
+fi
+
+# g++ -DPART?=true "$1".cpp -o out -Winvalid-pch -H &&
+# ./out <test.txt &&
+
+echo "Part 1:"
+g++ -DPART1=true "$1".cpp -o out -Winvalid-pch &&
+    ./out <"$1".txt && rm -f out
+
+echo "Part 2:"
+g++ -DPART2=true "$1".cpp -o out -Winvalid-pch &&
+    ./out <"$1".txt && rm -f out
