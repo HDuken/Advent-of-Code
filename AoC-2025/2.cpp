08bc02ef0be8bae677ff3a541c6cb9e4a26a04c3
Huu Duc Nguyen <tech.hodapp@simplelogin.com>
2025-12-04 16:21:24 +0100

    Draft solution for day 2

--- AoC-2025/2.cpp
+++ AoC-2025/2.cpp
@@ -4,28 +4,54 @@
  */
 
 #include "../pch.hpp"
+#include <utility>
 
-int ans = 0;
-int n_row = 0, n_col = 0;
-pii ds[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+ll ans = 0;
 stringstream ss;
+vector<pair<ll, ll>> ranges;
+
+bool is_invalid(ll x) {
+    string s = to_string(x);
+
+    // If the number has odd no. digits
+    if (s.size() % 2 != 0) return false;
+
+    string s1 = s.substr(0, s.size() / 2);
+    string s2 = s.substr(s.size() / 2, s.size() / 2);
+    if (s1 == s2) return true;
+
+    return false;
+}
 
 void read_input() {
     string line;
-    while (getline(cin, line)) {
-        LOG(line);
-        ss.clear();
-        ss.str(line);
+    getline(cin, line);
+    line.push_back(',');
+
+    ss.str(line);
+    char c;
+    ll a, b;
+
+    while (ss >> a >> c >> b >> c) {
+        ranges.push_back(pair<ll, ll>(a, b));
     }
 }
 
 void solve() {
-    // TODO(Duck): duck
+    ll a, b;
+    for (const auto &[a, b] : ranges) {
+        print(a, b);
+        for (ll i = a; i <= b; i++) {
+            if (is_invalid(i)) ans += i;
+        }
+    }
 }
 
 int main(int argc, char const *argv[]) {
     read_input();
+#if !PART2
     solve();
+#endif  // !PART2
     LOG(ans);
     return 0;
 }
